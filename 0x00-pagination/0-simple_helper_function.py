ca650ee405c227f3a484242e8a0b3fa688851d0e
foash-111 <mr2221@fayoum.edu.eg>
2024-08-19 12:21:25 +0300

    pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-"""pagination, parse the large response into small peices"""
+"""Pagination:
+is the process of separating print or digital content into discrete pages."""
 
 from typing import Tuple
 
 
-def index_range(page: int = 0, page_size: int = 0) -> Tuple:
+def index_range(page: int = 0, page_size: int = 0) -> Tuple[int, int]:
     """The function should return a tuple of size two
     containing a start index and an end index
     corresponding to the range of indexes to return
--- 0x00-pagination/README.md
+++ 0x00-pagination/README.md
@@ -1,1 +1,2 @@
 0x00-pagination
+Pagination is the process of separating print or digital content into discrete pages.
