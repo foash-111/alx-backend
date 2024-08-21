90228f148cc32e8cddb2425a069f10362dce8892
foash-111 <mr2221@fayoum.edu.eg>
2024-08-21 10:07:29 +0300

    implement MRU

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -19,14 +19,19 @@ class MRUCache(BaseCaching):
     Mostst Recently Used
     is the first to be removed
     """
+    def __init__(self):
+        """overide cache_data, make it OrderdDict to handle insertion order"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
     def put(self, key, item):
         """add item and consider update value for exist key as accessed one"""
         if not (key is None or item is None):
             if key in self.cache_data.keys():
                 del self.cache_data[key]
 
-            if len(self.cache_data) == self.MAX_ITEMS:
-                old_key, old_value = self.cache_data.popitem()
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                old_key, old_value = self.cache_data.popitem(last=True)
                 print('DISCARD: ', old_key)
 
             self.cache_data[key] = item
