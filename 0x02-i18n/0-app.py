12380660b6fe9ace3b25c8a539e2d3cb112e075e
foash-111 <mr2221@fayoum.edu.eg>
2024-08-28 11:45:04 +0300

    just a recab on flask

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""just recab on flask framework by implement basic web application"""
 
 from flask import Flask, render_template
 
@@ -8,6 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    """render simple page"""
     return render_template('0-index.html')
 
 
