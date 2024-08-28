b317d905932bb07c072c71556b63fb059de72233
foash-111 <mr2221@fayoum.edu.eg>
2024-08-28 15:57:38 +0300

    best_match which language

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""
+A simple Flask application demonstrating the use of Flask-Babel
+for internationalization (i18n) and localization (l10n).
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext
+
+app = Flask(__name__)
+babel = Babel(app=app)
+
+# Apply the Config class to the app configuration
+# app.config.from_object('Config')
+
+
+class Config:
+    """
+    customize Flask-Babel's behavior by configuring some options
+    This sets the default language for your application.
+    If no specific language is provided in the URL
+    or the user's settings, this locale will be used.
+    """
+    # app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    LANGUAGES = ["en", "fr"]
+
+
+@app.route('/')
+def hello():
+    """render simple page"""
+    greeting = gettext('Hello world')
+    return render_template('1-index.html', greeting=greeting)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Determine the best match for supported languages.
+
+    This function checks the `Accept-Language` header sent by the client
+    and selects the best match from the supported languages
+    defined in the `Config` class.
+
+    Returns:
+        str: The best match language code (e.g., 'en' or 'fr').
+    """
+    # request.accept_languages.best_match returns
+    # the best match for the supported languages
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='localhost', port=5000)
